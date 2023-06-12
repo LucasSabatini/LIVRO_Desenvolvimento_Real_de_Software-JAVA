1a9905936899dc6fe2824bb3e894eb64245e3e18
lucassabatini <lucas.frsabatini@gmail.com>
2023-06-12 16:57:39 -0300

    Ex2.6

---
+++ Cap02_Analista_de_Extratos_Bancarios_v2.6/src/main/BankTransactionAnalyzer.java
@@ -0,0 +1,28 @@
+package main;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.Month;
+import java.util.List;
+
+import static main.BankStatementCSVParser.calculateTotalAmount;
+import static main.BankStatementCSVParser.selectInMonth;
+
+public class BankTransactionAnalyzer {
+    private static final String RESOURCES = "/src/main/resources/";
+
+    public static void main(final String... args) throws IOException {
+        final BankStatementCSVParser bankStatementParser = new BankStatementCSVParser();
+
+        final String fileName = args[0];
+        final Path path = Paths.get(RESOURCES + fileName);
+        final List<String> lines = Files.readAllLines(path);
+
+        final List<BankTransaction> bankTransactions = bankStatementParser.parseLinesFromCSV(lines);
+
+        System.out.println("The total for all transactions is " + calculateTotalAmount(bankTransactions));
+        System.out.println("Transactions in January " + selectInMonth(bankTransactions, Month.JANUARY));
+    }
+}
