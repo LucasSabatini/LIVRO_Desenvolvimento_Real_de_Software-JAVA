fa764b9ea593a8d1d1da4f7819d7a7c57766ef75
lucassabatini <lucas.frsabatini@gmail.com>
2023-07-08 21:47:49 -0300

    Ex2.12 - Dissociando o Analista de Extratos Bancários do parser

---
+++ Cap02_Analista_de_Extratos_Bancarios_v2.12/src/main/BankStatementAnalyzer.java
@@ -0,0 +1,33 @@
+package main;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.Month;
+import java.util.List;
+
+public class BankStatementAnalyzer {
+
+    private static final String RESOURCES = "C:/Users/lucas/OneDrive/Área de Trabalho/Programação/Livros/Desenvolvimento Real de Software - Java/Cap02_Analista_de_Extratos_Bancarios_v2.12/src/main/resources/";
+
+    public void analyze(final String fileName, final BankStatementParser bankStatementParser)
+            throws IOException {
+
+        final Path path = Paths.get(RESOURCES + fileName);
+        final List<String> lines = Files.readAllLines(path);
+
+        final List<BankTransaction> bankTransactions = bankStatementParser.parseLinesFrom(lines);
+
+        final BankStatementProcessor bankStatementProcessor = new BankStatementProcessor(bankTransactions);
+
+        collectSummary(bankStatementProcessor);
+    }
+
+    private static void collectSummary(final BankStatementProcessor bankStatementProcessor) {
+        System.out.println("The total for all transactions is " + bankStatementProcessor.calculateTotalAmount());
+        System.out.println("The total for transactions in January is " + bankStatementProcessor.calculateTotalInMonth(Month.JANUARY));
+        System.out.println("The total for transactions in February is " + bankStatementProcessor.calculateTotalInMonth(Month.FEBRUARY));
+        System.out.println("The total salary received is " + bankStatementProcessor.calculateTotalForCategory("Salary"));
+    }
+}
