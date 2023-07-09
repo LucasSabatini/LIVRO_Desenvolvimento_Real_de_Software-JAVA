13009587c3c6610ba6fb81f1264f43185583116e
lucassabatini <lucas.frsabatini@gmail.com>
2023-07-08 21:55:03 -0300

    Ex2.13 - A aplicação principal a executar

---
+++ Cap02_Analista_de_Extratos_Bancarios_v2.13/src/main/BankStatementParser.java
@@ -0,0 +1,8 @@
+package main;
+
+import java.util.List;
+
+public interface BankStatementParser {
+    BankTransaction parseFrom(String line);
+    List<BankTransaction> parseLinesFrom(List<String> lines);
+}
---
+++ Cap02_Analista_de_Extratos_Bancarios_v2.13/src/main/BankTransactionParser.java
@@ -0,0 +1,19 @@
+package main;
+
+public class BankTransactionParser {
+
+    public BankTransaction parseFromCSV(final String line) {
+        // ...
+        throw new UnsupportedOperationException();
+    }
+
+    public BankTransaction parseFromJSON(final String line) {
+        // ...
+        throw new UnsupportedOperationException();
+    }
+
+    public BankTransaction parseFromXML(final String line) {
+        // ...
+        throw new UnsupportedOperationException();
+    }
+}
