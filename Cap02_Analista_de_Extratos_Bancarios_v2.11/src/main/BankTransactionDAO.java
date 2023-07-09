b81bba48bbcec66f0a59c469eff4203ad42f6ad0
lucassabatini <lucas.frsabatini@gmail.com>
2023-07-08 21:34:00 -0300

    Ex2.11 - Introduzindo uma interface para analisar sintaticamente extratos bancários

---
+++ Cap02_Analista_de_Extratos_Bancarios_v2.11/src/main/BankTransactionDAO.java
@@ -0,0 +1,26 @@
+package main;
+
+import java.time.LocalDate;
+
+public class BankTransactionDAO {
+
+    public BankTransaction create(final LocalDate date, final double amount, final String description) {
+        // ...
+        throw new UnsupportedOperationException();
+    }
+
+    public BankTransaction read(final long id) {
+        // ...
+        throw new UnsupportedOperationException();
+    }
+
+    public BankTransaction update(final long id) {
+        // ...
+        throw new UnsupportedOperationException();
+    }
+
+    public BankTransaction delete(final long id) {
+        // ...
+        throw new UnsupportedOperationException();
+    }
+}
