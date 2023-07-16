f58f63706d9bc301d51c45a3e8dbd4cbb5578344
lucassabatini <lucas.frsabatini@gmail.com>
2023-07-16 20:11:54 -0300

    Ex2.14 - Uma unidade de teste falha para o parser CSV

---
+++ Cap02_Analista_de_Extratos_Bancarios/Cap02_Analista_de_Extratos_Bancarios_v2.14/src/main/java/main/BankStatementCSVParser.java
@@ -0,0 +1,28 @@
+package main;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class BankStatementCSVParser implements BankStatementParser {
+
+    private static final DateTimeFormatter DATE_PATTERN = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+
+    @Override
+    public BankTransaction parseFrom(final String line) {
+        final String[] columns = line.split(",");
+
+        final LocalDate date = LocalDate.parse(columns[0], DATE_PATTERN);
+        final double amount = Double.parseDouble(columns[1]);
+        final String description = columns[2];
+
+        return new BankTransaction(date,amount,description);
+    }
+
+    @Override
+    public List<BankTransaction> parseLinesFrom(final List<String> lines) {
+        return lines.stream().map(this::parseFrom).collect(Collectors.toList());
+    }
+
+}
---
+++ Cap02_Analista_de_Extratos_Bancarios/Cap02_Analista_de_Extratos_Bancarios_v2.14/src/test/java/BankStatementCSVParserTest.java
@@ -0,0 +1,14 @@
+import main.BankStatementCSVParser;
+import main.BankStatementParser;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class BankStatementCSVParserTest {
+
+    private final BankStatementParser statementParser = new BankStatementCSVParser();
+
+    @Test
+    public void shouldParseOneCorrectLine() throws Exception {
+        Assert.fail("Not yet implemented");
+    }
+}
