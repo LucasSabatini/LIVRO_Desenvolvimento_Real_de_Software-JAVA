46367e313fff9fbdd66181ef6499db1fc392766d
lucassabatini <lucas.frsabatini@gmail.com>
2023-07-17 19:36:07 -0300

    Ex3.1 - Encontre as transações bancárias acima de certo montante

---
+++ Cap03_Estendendo_o_Analista_de_Extratos_Bancarios/Cap03_Estendendo_o_Analista_de_Extratos_Bancarios_v3.1/src/main/java/BankStatementProcessor.java
@@ -0,0 +1,50 @@
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BankStatementProcessor {
+
+    private final List<BankTransaction> bankTransactions;
+
+    public BankStatementProcessor(final List<BankTransaction> bankTransactions) {
+        this.bankTransactions = bankTransactions;
+    }
+
+    public double calculateTotalAmount() {
+        double total = 0d;
+        for(final BankTransaction bankTransaction: bankTransactions) {
+            total += bankTransaction.getAmount();
+        }
+        return total;
+    }
+
+    public double calculateTotalInMonth(final Month month) {
+        double total = 0d;
+        for(final BankTransaction bankTransaction: bankTransactions) {
+            if(bankTransaction.getDate().getMonth() == month) {
+                total += bankTransaction.getAmount();
+            }
+        }
+        return total;
+    }
+
+    public double calculateTotalForCategory(final String category) {
+        double total = 0d;
+        for(final BankTransaction bankTransaction: bankTransactions) {
+            if(bankTransaction.getDescription().equals(category)) {
+                total += bankTransaction.getAmount();
+            }
+        }
+        return total;
+    }
+
+    public List<BankTransaction> findTransactionsGreaterThanEqual(final int amount) {
+        final List<BankTransaction> result = new ArrayList<>();
+        for(final BankTransaction bankTransaction : bankTransactions) {
+            if(bankTransaction.getAmount() >= amount) {
+                result.add(bankTransaction);
+            }
+        }
+        return result;
+    }
+}
