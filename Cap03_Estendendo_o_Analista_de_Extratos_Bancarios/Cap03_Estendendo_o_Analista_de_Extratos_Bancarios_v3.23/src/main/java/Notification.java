a25a81f1e2fbf1f38f71f5fea332a2186d8c867d
lucassabatini <lucas.frsabatini@gmail.com>
2023-07-22 23:29:54 -0300

    Ex3.23 Evite exceções específicas da implementação

---
+++ Cap03_Estendendo_o_Analista_de_Extratos_Bancarios/Cap03_Estendendo_o_Analista_de_Extratos_Bancarios_v3.23/src/main/java/Notification.java
@@ -0,0 +1,25 @@
+import javax.xml.transform.Source;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Notification {
+    private final List<String> errors = new ArrayList<>();
+
+    public void addError(final String message) {
+        errors.add(message);
+    }
+
+    public boolean hasErrors() {
+        return !errors.isEmpty();
+    }
+
+    public String errorMessage() {
+        return errors.toString();
+    }
+
+    public List<String> getErrors() {
+        return this.errors;
+    }
+
+    public String read(final Source source) throws OracleException { ... };
+}
