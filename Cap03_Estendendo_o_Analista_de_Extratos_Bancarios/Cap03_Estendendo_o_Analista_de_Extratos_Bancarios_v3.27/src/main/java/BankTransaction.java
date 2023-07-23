d06efb04a2953714d1876ddb6221cbec589e844f
lucassabatini <lucas.frsabatini@gmail.com>
2023-07-22 23:55:32 -0300

    Ex3.27 - Arquivo de construção build.gradle em Gradle

---
+++ Cap03_Estendendo_o_Analista_de_Extratos_Bancarios/Cap03_Estendendo_o_Analista_de_Extratos_Bancarios_v3.27/src/main/java/BankTransaction.java
@@ -0,0 +1,78 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+import java.util.Objects;
+
+public class BankTransaction {
+    private final LocalDate date;
+    private final double amount;
+    private final String description;
+
+    public BankTransaction(final LocalDate date, final double amount, final String description) {
+        this.date = date;
+        this.amount = amount;
+        this.description = description;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Notification validate() {
+        final Notification notification = new Notification();
+        if(this.description.length() > 100) {
+            notification.addError("The description is too long");
+        }
+
+        final LocalDate parsedDate;
+        try {
+            parsedDate = LocalDate.parse(this.date);
+            if(parsedDate.isAfter(LocalDate.now())) {
+                notification.addError("Date cannot be in the future");
+            }
+        }
+        catch (DateTimeParseException e) {
+            notification.addError("Invalid format for date");
+        }
+
+        final double amount;
+        try {
+            amount = Double.parseDouble(this.amount);
+        }
+        catch (NumberFormatException e) {
+            notification.addError("Invalid format for amount");
+        }
+        return notification;
+    }
+
+    @Override
+    public String toString() {
+        return "BankTransaction{" +
+                "date=" + date +
+                ", amount=" + amount +
+                ", description='" + description + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BankTransaction that = (BankTransaction) o;
+        return Double.compare(that.amount, amount) == 0 &&
+                date.equals(that.date) &&
+                description.equals(that.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(date, amount, description);
+    }
+}
