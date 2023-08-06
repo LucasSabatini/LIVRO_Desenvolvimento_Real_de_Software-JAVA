b41c630533721e57ed02ee22db6d28aeaa21fcc6
Lucas Sabatini <lucas.frsabatini@gmail.com>
2023-08-06 14:29:31 -0300

    Correções no repositório

--- Cap02_Analista_de_Extratos_Bancarios_v2.11/src/main/BankStatementParser.java
+++
@@ -1,8 +0,0 @@
-package main;
-
-import java.util.List;
-
-public interface BankStatementParser {
-    BankTransaction parseFrom(String line);
-    List<BankTransaction> parseLinesFrom(List<String> lines);
-}
